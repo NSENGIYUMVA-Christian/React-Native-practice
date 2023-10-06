4b27230fa792c2c997934dcb5cf89aa9aaf6a177
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-10-06 12:45:39 +0300

    spliting codes into components

---
+++ AwesomeProject/components/GoalItem.js
@@ -0,0 +1,24 @@
+import { View, Text, StyleSheet } from "react-native";
+function GoalItem(props) {
+  return (
+    <View style={styles.goalItem}>
+      <Text style={styles.goalText}>{props.text}</Text>
+    </View>
+  );
+}
+
+export default GoalItem;
+
+///////////////styling
+
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "green",
+  },
+  goalText: {
+    color: "white",
+  },
+});
